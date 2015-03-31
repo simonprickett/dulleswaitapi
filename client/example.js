d9e683853c34da9a504342745a743e126ffa9378
Simon Prickett <simon@crudworks.org>
2015-03-30 22:27:43 -0400

    Simple API client demo.

--- client/example.js
+++ client/example.js
@@ -1,27 +1,32 @@
-window.onload = function() {
-	$.ajax({
-		url: "http://dulleswaitapi.crudworks.org/",
-		success: function( data ) {
-			var dullesData = data.airports[0],
-				numCheckPoints = data.airports[0].checkPoints.length,
-				eastWait = -1,
-				westWait = -1,
-				updatedAt = -1,
-				n = 0,
-				thisCheckPoint = undefined;
+var myApp = {
+	updateWaitTime: function() {
+		$('#eastwaittime').html('?');
+		$('#westwaittime').html('?');
+		$.ajax({
+			url: "http://dulleswaitapi.crudworks.org/",
+			success: function( data ) {
+				var dullesData = data.airports[0],
+					numCheckPoints = data.airports[0].checkPoints.length,
+					eastWait = -1,
+					westWait = -1,
+					updatedAt = -1,
+					n = 0,
+					thisCheckPoint = undefined;
 
-			updatedAt = dullesData.lastUpdatedTimestamp;
-			for (n = 0; n < numCheckPoints; n++) {
-				thisCheckPoint = dullesData.checkPoints[n];
-				if (thisCheckPoint.name === 'East') {
-					console.log('East: ' + thisCheckPoint.wait);
-					$('#eastwaittime').html(thisCheckPoint.wait);
-				} else {
-					console.log('West: ' + thisCheckPoint.wait);
-					$('#westwaittime').html(thisCheckPoint.wait);
+				for (n = 0; n < numCheckPoints; n++) {
+					thisCheckPoint = dullesData.checkPoints[n];
+					if (thisCheckPoint.name === 'East') {
+						$('#eastwaittime').html(thisCheckPoint.wait);
+					} else {
+						$('#westwaittime').html(thisCheckPoint.wait);
+					}
 				}
 			}
-			console.log(updatedAt);
-		}
-	});
+		});
+	}
+};
+
+window.onload = function() {
+	myApp.updateWaitTime();
+	window.setInterval(myApp.updateWaitTime, 120000);
 }
